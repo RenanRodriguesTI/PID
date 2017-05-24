d619869c225755f2d9457daf15fc3a3fac7174ee
RenanRodriguesTI <renanrodriguesmachado2@gmail.com>
2017-05-24 17:24:40 -0300

    Add files via upload
    Cadastro de romaneio formulario básico

--- cadastroromaneio.php
+++ cadastroromaneio.php
@@ -7,7 +7,23 @@
 <body>
 <form action="cadastroromaneio.php" method="post">
 <h3>Cadastro de romaneio</h3>
+<label>Código da encomenda</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>Observação</label><br>
 
+<input type="text" name="codencom" value=""/>&nbsp;<input type="text" name="obsencom"/><br>
+
+<label>Codigo cidade origem</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>Cidade de Origem</label><br>
+
+<input type="text" name="codcidorigem" value=""/>&nbsp;<input type="text" name="nomecidorigem"/><br>
+
+<label>Codigo cidade destino</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>Cidade de Destino</label><br>
+<input type="text" name="codciddest" value=""/>&nbsp;<input type="text" name="nomeciddest"/><br>
+
+<label>Codigodo Motorista</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>Nome do funcionário</label><br>
+
+<input type="text" name="codfun" value=""/>&nbsp;<input type="text" name="nomefun"/><br>
+
+<label>Codigo do Caminhão<label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>Placa do caminhão</label><br>
+<input type="text" name="codcaminhao" value=""/>&nbsp;<input type="text" name="placadocaminhao"/><br>
 <label>Data de cadastro</label><br>
 
 <input type="date" name="data"/><br>
